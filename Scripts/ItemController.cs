1d9b70302850c90c3ce00e77470e6906196e1918
arshfrogrice <blackabyss1116@gmail.com>
2025-01-24 21:26:15 +0530

    Delete Scripts directory

--- Scripts/ItemController.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class ItemController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Item Item;
-
-}
